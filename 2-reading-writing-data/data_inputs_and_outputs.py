bb28fbc4081e83262358d03417a5399b6be4b240
Patricia Carrasco <patri.carrasco@gmail.com>
2023-12-21 11:45:04 +0100

    add rd-wr

--- 2-reading-writing-data/data_inputs_and_outputs.py
+++ 2-reading-writing-data/data_inputs_and_outputs.py
@@ -16,29 +16,35 @@ spark.sparkContext.setLogLevel("ERROR")
 
 # Let's check if the change went through
 
-spark.sparkContext.getConf().getAll()
-"""
-El código spark.sparkContext.getConf().getAll() en PySpark se utiliza para obtener todas las configuraciones asociadas 
-con el contexto de Spark. Aquí hay una explicación detallada de cada parte:
+print(
+    spark.sparkContext.getConf().getAll()
+)
 
-- spark: Es una instancia de SparkSession, que es la entrada principal para interactuar con Spark en el contexto de SQL 
-y DataFrame.
+# Load a JSON file into a Spark DataFrame called user_log.
+df = spark.read.json("../data/sparkify_log_small.json")
+df.show(2)
 
-- sparkContext: Es el contexto de Spark asociado con la instancia de SparkSession. El SparkContext es responsable de 
-la conexión con el clúster de Spark y de la coordinación de las operaciones.
+# Print the schema with the printSchema() method.
+df.printSchema()
 
-- getConf(): Es un método del SparkContext que devuelve un objeto SparkConf. SparkConf es la clase que contiene 
-la configuración de Spark, incluyendo las configuraciones del clúster, la aplicación, y otras opciones de configuración.
+# Try the describe() method to see what we can learn from our data.
 
-- getAll(): Es un método del objeto SparkConf que devuelve todas las configuraciones presentes en ese objeto 
-como una lista de pares clave-valor.
+print(df.describe())
 
-En resumen, spark.sparkContext.getConf().getAll() te proporcionará una lista de todas las configuraciones actualmente 
-configuradas para tu aplicación Spark, lo cual puede ser útil para verificar la configuración en tiempo de ejecución. 
-Esto puede incluir configuraciones predeterminadas, así como configuraciones que hayas establecido explícitamente en 
-tu código. Puedes imprimir o analizar esta lista para obtener información detallada sobre la configuración de Spark en 
-tu aplicación.
 
-"""
 
-print(spark.sparkContext.getConf().getAll())
+# Use the take() method to grab the first few records.
+
+print(df.take(5))
+
+# Save it into a different format, for example, into a CSV file, with the write.save() method
+df.write.mode("overwrite").save("../data/output.csv", format="csv", header=True)
+
+# Use the read.csv method
+df_csv = spark.read.csv("../data/output.csv",header = True, inferSchema = True)
+df_csv.printSchema()
+print(df_csv.take(1))
+
+
+# Show the userID column for the first several rows
+df_csv.select("userID").show()
